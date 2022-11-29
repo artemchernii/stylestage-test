ce85d30c164a0ca41c569a011691bc63b24c74d4
Artem Chernii <104429440+artemcherniiosf@users.noreply.github.com>
2022-11-29 14:19:23 +0000

    Squashed commit of the following:
    commit ca3dfb0126a17acc6c5c382392666d6a55171195
    Merge: c5265c6 4c4fc16
    Author: Artem Chernii <104429440+artemcherniiosf@users.noreply.github.com>
    Date:   Tue Nov 29 13:57:05 2022 +0000
    
        Merge branch 'features/#2-stylestage-refactor' of github.com-personal:artemchernii/stylestage-test into features/#2-stylestage-refactor
    
    commit c5265c6c0837746cd865e6b2e887f109240b1583
    Author: Artem Chernii <104429440+artemcherniiosf@users.noreply.github.com>
    Date:   Tue Nov 29 13:52:20 2022 +0000
    
        chore: add comments to browserandevents.js
    
    commit 4c4fc16703c734f1af0e9d59800bfbbffb7be306
    Author: Artem Chernii <104429440+artemcherniiosf@users.noreply.github.com>
    Date:   Tue Nov 29 13:52:20 2022 +0000
    
        chore: add comments to browserandevents.js
    
    commit efe20e4146dd2429ba41416be6294e236155b4d0
    Author: Artem Chernii <104429440+artemcherniiosf@users.noreply.github.com>
    Date:   Sun Nov 20 23:54:59 2022 +0000
    
        feat: playing with browser events
    
    commit 3b4d12f1a87f51623a53d10560a84c2a78cffc46
    Merge: ae132b8 bac384e
    Author: Artem Cherniy <artemcherniy7@gmail.com>
    Date:   Mon Oct 31 14:27:23 2022 +0100
    
        Merge pull request #1 from artemchernii/features/#1-small-yt-practice
    
        Features/#1 small yt practice

---
+++ browserandevents.js
@@ -0,0 +1,61 @@
+console.log('WINDOW: ', window);
+console.log('localStorage', localStorage);
+
+const addItemToStorage = (item) => {
+	window.localStorage.setItem('name', JSON.stringify(item));
+	rewriteHtml();
+};
+
+function rewriteHtml() {
+	heading.setAttribute('data-name', localItem.name);
+	heading.setAttribute('data-nationality', localItem.nationality);
+}
+
+const localItem = JSON.parse(localStorage.getItem('name'));
+
+const heading = document.createElement('h1');
+
+heading.classList.add(localItem.name);
+heading.innerHTML = `${localItem.name} is ${localItem.nationality}`;
+heading.style.cssText = 'letter-spacing: 2px; font-size: 42px';
+heading.setAttribute('data-name', localItem.name);
+heading.setAttribute('data-nationality', localItem.nationality);
+
+const appending = (child, parent = 'body') => {
+	document.body.querySelector(parent).appendChild(child);
+	console.log(`We append ${child} to ${parent}`);
+};
+
+appending(heading, 'main');
+
+const changeName = document.querySelector('#change-name');
+changeName.style.marginTop = '20px';
+const onChangeName = (e) => {
+	e.preventDefault();
+	console.log(e);
+	console.log(e.target);
+
+	localItem.name === 'Teresa'
+		? addItemToStorage({ name: 'Artem', nationality: 'Ukrainian' })
+		: addItemToStorage({ name: 'Teresa', nationality: 'German' });
+};
+
+changeName.addEventListener('click', onChangeName);
+
+('use strict');
+
+//Closure practice
+
+//Closure practice
+
+function changeBalance() {
+	let balance = 0;
+	return function (sum) {
+		balance += sum;
+		console.log('balance: ', balance);
+	};
+}
+
+const change = changeBalance();
+change(20);
+change(40);
